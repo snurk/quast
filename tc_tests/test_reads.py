bce55e1fa2062843460e566ecf2852c337a34c2c
almiheenko <al.miheenko@gmail.com>
2015-03-30 22:09:45 +0300

    fix test

--- tc_tests/test_reads.py
+++ tc_tests/test_reads.py
@@ -4,4 +4,4 @@ from common import *
 name = os.path.basename(__file__)[5:-3]
 
 run_quast(name, contigs=[contigs_1k_2], params='-1 ' + reads_1)
-assert_metric(name, '# reads', ['56320'])
+assert_metric(name, '# reads', ['28160'])
