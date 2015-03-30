7784d17e99f2acab90fe7d9175e17e45c052a86b
almiheenko <al.miheenko@gmail.com>
2015-03-30 22:36:54 +0300

    fix test

--- tc_tests/test_conservative_genes.py
+++ tc_tests/test_conservative_genes.py
@@ -4,4 +4,4 @@ from common import *
 name = os.path.basename(__file__)[5:-3]
 
 run_quast(name, contigs=[contigs_1k_2], params='--gene-finding --eukaryote')
-assert_metric(name, 'Complete core genes', ['0'])
+assert_metric(name, 'Complete core genes (%)', ['0'])
