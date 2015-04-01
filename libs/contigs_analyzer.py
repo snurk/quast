793f2f6b55e471321dd9749171f2a5eb9f530cfb
almiheenko <al.miheenko@gmail.com>
2015-04-01 11:48:54 +0300

    add misassembly_info file

--- libs/contigs_analyzer.py
+++ libs/contigs_analyzer.py
@@ -628,6 +628,7 @@ def plantakolya(cyclic, index, contigs_fpath, nucmer_fpath, output_dirpath, ref_
         #Recording contig stats
         ctg_len = len(seq)
         print >> planta_out_f, 'CONTIG: %s (%dbp)' % (contig, ctg_len)
+
         #Check if this contig aligned to the reference
         if contig in aligns:
             #Pull all aligns for this contig
