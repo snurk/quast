6796216a4fc63d4981e870eefbb04efd31576c42
Sergey Nurk <sergeynurk@gmail.com>
2015-10-06 21:07:34 +0300

    small fix

--- libs/plotter.py
+++ libs/plotter.py
@@ -79,19 +79,17 @@ dict_color_and_ls = {}
 def save_colors_and_ls(fpaths):
     if not dict_color_and_ls:
         color_id = 0
-        next_color_id = color_id
-        ls = primary_line_style
         for fpath in fpaths:
+            ls = primary_line_style
             label = qutils.label_from_fpath(fpath)
             # contigs and scaffolds should be equally colored but scaffolds should be dashed
             if fpath and fpath in qconfig.dict_of_broken_scaffolds:
                 color = dict_color_and_ls[qutils.label_from_fpath(qconfig.dict_of_broken_scaffolds[fpath])][0]
                 ls = secondary_line_style
             else:
-                 next_color_id += 1
                  color = colors[color_id % len(colors)]
+                 color_id += 1
             dict_color_and_ls[label] = (color, ls)
-            color_id = next_color_id
 
 
 def get_color_and_ls(fpath):
